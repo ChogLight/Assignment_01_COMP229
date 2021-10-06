6d6a2e7bd12aa958e701b71f4a875246e7300f3e
ChogLight <sebas8812@gmail.com>
2021-10-05 22:31:45 -0400

    updated index.ts and index.js files

--- routes/index.ts
+++ routes/index.ts
@@ -4,25 +4,25 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req:express.Request, res:express.Response, next:express.NextFunction) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page:'home' });
 });
 router.get('/home', function(req:express.Request, res:express.Response, next:express.NextFunction) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page:'home' });
 });
 /* GET about me page. */
 router.get('/about', function(req:express.Request, res:express.Response, next:express.NextFunction) {
-  res.render('index', { title: 'About me' });
+  res.render('index', { title: 'About me', page:'about' });
 });
 /* GET projects page. */
 router.get('/projects', function(req:express.Request, res:express.Response, next:express.NextFunction) {
-  res.render('index', { title: 'Projects' });
+  res.render('index', { title: 'Projects', page:'projects' });
 });
 /* GET services page. */
 router.get('/services', function(req:express.Request, res:express.Response, next:express.NextFunction) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Services', page: 'services' });
 });
 /* GET contact me page. */
 router.get('/contact', function(req:express.Request, res:express.Response, next:express.NextFunction) {
-  res.render('index', { title: 'Contact me' });
+  res.render('index', { title: 'Contact me', page: 'contact' });
 });
 export default router;
